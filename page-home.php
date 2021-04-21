a7cd9e7772388dbb11c916ccdb5a687f4064f91d
Lincoln <lincoolngomes@gmail.com>
2021-04-21 19:38:08 -0300

    Update img page-home

--- page-home.php
+++ page-home.php
@@ -6,12 +6,12 @@ get_header();
 <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
   <section class="introducao">
     <div class="container">
-      <h1 data-anime="400" class="fadeInDown">Bicicletas feitas a mão</h1>
+      <h1 data-anime="400" class="fadeInDown"><?php the_field('titulo_introducao'); ?></h1>
       <blockquote data-anime="800" class="fadeInDown quote-externo">
-        <p>"não tenha nada em sua casa que você não considere útil ou acredita ser bonito"</p>
-        <cite>William Morris</cite>
+        <p><?php the_field('quote_introducao'); ?></p>
+        <cite><?php the_field('citacao_introducao'); ?></cite>
       </blockquote>
-      <a data-anime="1200" href="produtos.html" class="fadeInDown btn">Orçamento</a>
+      <a data-anime="1200" href="/produtos" class="fadeInDown btn">Orçamento</a>
     </div>
   </section>
 
@@ -20,7 +20,7 @@ get_header();
     <ul class="produtos_lista">
       <li class="grid-1-3">
         <div class="produtos_icone">
-          <img src="./img/produtos/passeio.svg" alt="Bikecraft Passeio">
+          <img src="<?php echo get_template_directory_uri(); ?>/img/produtos/passeio.svg" alt="Bikecraft Passeio">
         </div>
 
         <h3>Passeio</h3>
@@ -28,22 +28,22 @@ get_header();
       </li>
       <li class="grid-1-3">
         <div class="produtos_icone">
-          <img src="./img/produtos/esporte.svg" alt="Bikecraft Esporte">
+          <img src="<?php echo get_template_directory_uri(); ?>/img/produtos/esporte.svg" alt="Bikecraft Esporte">
         </div>
         <h3>Esporte</h3>
         <p>Mais rápida do que Forrest Gump, ninguém vai pegar você.</p>
       </li>
       <li class="grid-1-3">
         <div class="produtos_icone">
-          <img src="./img/produtos/retro.svg" alt="Bikecraft Retrô">
+          <img src="<?php echo get_template_directory_uri(); ?>/img/produtos/retro.svg" alt="Bikecraft Retrô">
         </div>
         <h3>Retrô</h3>
         <p>O passado volta para lembrarmos o que devemos fazer no futuro.</p>
       </li>
     </ul>
     <div class="call-to-action">
-      <p>clique aqui e veja os detalhes dos produtos</p>
-      <div><a href="produtos.html" class="btn btn-preto">Produtos</a></div>
+      <p><?php the_field('chamada_produtos'); ?></p>
+      <div><a href="/produtos" class="btn btn-preto">Produtos</a></div>
     </div>
 
   </section>
@@ -52,13 +52,13 @@ get_header();
     <div class="container ">
       <h2 class="subtitulo">Portfólio</h2>
       <ul class="portfolio_lista">
-        <li class="grid-8"><img src="img/portfolio/retro.jpg" alt="Bicicleta Retrô"></li>
-        <li class="grid-8"><img src="img/portfolio/passeio.jpg" alt="Bicicleta Passeio"></li>
-        <li class="grid-16"><img src="img/portfolio/esporte.jpg" alt="Bicicleta Esporte"></li>
+        <li class="grid-8"><img src="<?php echo get_template_directory_uri(); ?>/img/portfolio/retro.jpg" alt="Bicicleta Retrô"></li>
+        <li class="grid-8"><img src="<?php echo get_template_directory_uri(); ?>/img/portfolio/passeio.jpg" alt="Bicicleta Passeio"></li>
+        <li class="grid-16"><img src="<?php echo get_template_directory_uri(); ?>/img/portfolio/esporte.jpg" alt="Bicicleta Esporte"></li>
       </ul>
       <div class="call-to-action">
-        <p>conheça mais o nosso portfólio</p>
-        <div><a href="portfolio.html" class="btn">Portfólio</a></div>
+        <p><?php the_field('chamada_portfolio'); ?></p>
+        <div><a href="/portfolio" class="btn">Portfólio</a></div>
       </div>
     </div>
 
@@ -66,7 +66,7 @@ get_header();
 
   <section class="qualidade container">
     <h2 class="subtitulo">Qualidade</h2>
-    <img src="./img/bikcraft-qualidade.svg" alt="Bikcraft Qualidade">
+    <img src="<?php echo get_template_directory_uri(); ?>/img/bikcraft-qualidade.svg" alt="Bikcraft Qualidade">
     <ul class="qualidade_lista">
       <li class="grid-1-3">
         <h3>Durabilidade</h3>

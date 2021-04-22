2e771f124e29567126dc38fe7fc50085867fcb98
Lincoln <lincoolngomes@gmail.com>
2021-04-22 20:45:31 -0300

    add template qualidade

---
+++ inc/clientes-portfolio.php
@@ -0,0 +1,5 @@
+<ul class="portfolio_lista">
+        <li class="grid-8"><img src="<?php echo get_template_directory_uri(); ?>/img/portfolio/retro.jpg" alt="Bicicleta Retrô"></li>
+        <li class="grid-8"><img src="<?php echo get_template_directory_uri(); ?>/img/portfolio/passeio.jpg" alt="Bicicleta Passeio"></li>
+        <li class="grid-16"><img src="<?php echo get_template_directory_uri(); ?>/img/portfolio/esporte.jpg" alt="Bicicleta Esporte"></li>
+</ul>
--- page-sobre.php
+++ page-sobre.php
@@ -28,29 +28,7 @@ get_header();
 
 	</section>
 
-	<section class="qualidade container">
-		<h2 class="subtitulo">Qualidade</h2>
-		<img src="./img/bikcraft-qualidade.svg" alt="Bikcraft Qualidade">
-		<ul class="qualidade_lista">
-			<li class="grid-1-3">
-				<h3>Durabilidade</h3>
-				<p>Ainda assim, existem dúvidas a respeito de como a necessidade de renovação renovação renovação</p>
-			</li>
-			<li class="grid-1-3">
-				<h3>Design</h3>
-				<p>Ainda assim, existem dúvidas a respeito de como a necessidade de renovação renovação renovação</p>
-			</li>
-			<li class="grid-1-3">
-				<h3>Sustentabilidade</h3>
-				<p>Ainda assim, existem dúvidas a respeito de como a necessidade de renovação renovação renovação</p>
-			</li>
-		</ul>
-		<div class="call-to-action">
-			<p>conheça mais a nossa história</p>
-			<div><a href="sobre.html" class="btn btn-preto">Sobre</a></div>
-		</div>
-
-	</section>
+	<?php include(TEMPLATEPATH . "/inc/qualidade.php" ); ?>
 
 	<?php endwhile; else: endif; ?>
 
